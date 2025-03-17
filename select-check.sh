936b8192df45f7452bff0afb68fbb7a595ad9833
Chris McKenzie <kristopolous@yahoo.com>
2025-03-17 16:25:33 -0700

    tmux check

--- select-check.sh
+++ select-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 test=$(uuidgen)
-echo "Here's the code to check, I'll tell you what you're using:"
+echo "Here's the code to check, select it with the mouse and I'll tell you what clipboard it has populated:"
 echo -e "\n$test\n"
 while true; do
     flag=0
@@ -16,9 +16,11 @@ while true; do
         flag=1
         printf "Clipboard | "
     fi
-    if [[ "$(tmux show-buffer)" == "$test" ]]; then
-        flag=1
-        printf "tmux (internal) "
+    if pgrep tmux > /dev/null; then
+        if [[ "$(tmux show-buffer)" == "$test" ]]; then
+            flag=1
+            printf "tmux (internal) "
+        fi
     fi
     if [[ $flag == "0" ]]; then
         printf "None!"
