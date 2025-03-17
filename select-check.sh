4454c9cf22a3e5f66bb97b41889ab38d3a2fb883
Chris McKenzie <kristopolous@yahoo.com>
2025-03-17 16:19:48 -0700

    A way to check what mouse selection is doing

---
+++ select-check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+test=$(uuidgen)
+echo "Here's the code to check, I'll tell you what you're using:"
+echo -e "\n$test\n"
+while true; do
+    flag=0
+    if [[ "$(xsel -po)" == "$test" ]]; then
+        flag=1
+        printf "Primary | "
+    fi
+    if [[ "$(xsel -so)" == "$test" ]]; then
+        flag=1
+        printf "Secondary | "
+    fi
+    if [[ "$(xsel -bo)" == "$test" ]]; then
+        flag=1
+        printf "Clipboard | "
+    fi
+    if [[ "$(tmux show-buffer)" == "$test" ]]; then
+        flag=1
+        printf "tmux (internal) "
+    fi
+    if [[ $flag == "0" ]]; then
+        printf "None!"
+    fi
+    tput el && tput cr
+    sleep 1
+done
